ef2d7447acfd65cddcfbf0807db1b9140ceeaf51
mastrmind-dev <asapthaka@gmail.com>
2022-08-02 07:54:19 +0530

    first commit - created the basic structure of folders and files tree of the project folder and did page developing works

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <router-link to="/"
+      ><h1><span>Vue</span>Movies</h1></router-link
+    >
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+
+  &::selection {
+    background: transparentize($color: #42b883, $amount: 0.5);
+  }
 }
 
-nav {
-  padding: 30px;
+body {
+  background-color: #35495e;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0 0 0 6px rgba(0, 0, 0, 0.1);
 
-    &.router-link-exact-active {
-      color: #42b983;
+  h1 {
+    color: #fff;
+    font-size: 28px;
+
+    span {
+      color: #42b883;
     }
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img src="https://img.cdn-pictorem.com/uploads/collection/I/IB5PAB9RBI/900_Anime_7_1608090041.5705.jpg" alt="naruto-poster" class="featured-img">
+        <div class="detail">
+          <h3>Naruto</h3>
+          <p>Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for recognition and dreams of becoming the Hokage, the village's leader and strongest ninja.</p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
